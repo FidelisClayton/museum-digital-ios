93f2ffa23495cab5904dd01fe87c2500b8ca94bc
Clayton Fidelis <clayton.fidelis@live.com>
2020-05-01 18:37:39 +0200

    add gitignore

---
+++ museum-digital/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  museum-digital
+//
+//  Created by Clayton Fidelis on 01.05.20.
+//  Copyright © 2020 Clayton Fidelis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
